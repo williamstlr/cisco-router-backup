704c219ada1defca9ad3dc6cc756e31558be0a1e
williamstlr <williams.tlr@gmail.com>
2015-11-13 11:51:10 -0700

    Added argparse and made more robust
    Used argparse to parse my args. Also added a check to make sure backed
    up file existed and was greater than 0 bytes in size. Backup will now go
    into the cisco folder under the hostname/ip address on the ftp server.

--- cisco-router-backup.py
+++ cisco-router-backup.py
@@ -1,73 +1,89 @@
 #!/usr/bin/python
+#You need to successfully ssh into the device manusally to accept the hostkey once  for this to work.
 import pexpect
 import sys
 import re
 import time
 import os.path
+import syslog
+from subprocess import call
+import os
+import argparse
+
+
+#setup argument parser
+parser = argparse.ArgumentParser()
+parser.add_argument("device", help="Hostname or IP Address of device to be backed up")
+parser.parse_args()
+args = parser.parse_args()
 
 
 credentialsFile="./cred-file"
-
-#check to make sure we have a config file specified
-if len(sys.argv) < 2 :
-        print('USAGE: %s router-address' % (scriptName))
-        print('EXAMPLE: %s 192.168.1.216' % (scriptName))
-        exit(1)
-
-if os.path.isfile(credentialsFile) == False:
-        print("Credentials file not found")
-        exit(1)
-
-
+ftp_backup_location = "/home/netbackup/cisco/"
+date_time = time.strftime("%m.%d.%Y_%H:%M")
+syslog.syslog('Starting ' + parser.prog)
 
 #Read credentials in
 with open(credentialsFile) as f:
         credentials=f.readlines()
 
 
+#set a bunch of names
 router_user=credentials[0].rstrip()
 router_pass=credentials[1].rstrip()
-router = sys.argv[1].rstrip()
+router = args.device
+backup_file_name = router + "_" + date_time + ".cfg"
+backup_full_path = ftp_backup_location+router + "/" + backup_file_name
+
+#If the backup location dosen't exist for this device, create it and chown it
+if os.path.isdir(ftp_backup_location+router) == False:
+        syslog.syslog("Backup folder doesn't exist, creating it")
+        call(["mkdir", "-p",ftp_backup_location+router])
+        call(["chown","netbackup:netbackup",ftp_backup_location+router])
+
 
 
 #Attempt to connect to the router
+syslog.syslog("Attempting to login to " + router)
 try:
         child = pexpect.spawn('ssh %s@%s -1' % (router_user, router))
-        child.logfile = sys.stdout
+#       child.logfile = sys.stdout #uncomment to enable debugging
         child.timeout = 5
         child.expect('password:')
 
 except pexpect.TIMEOUT:
-        print ("Did not recieve login prompt")
+        syslog.syslog("Did not receive login prompt. Exiting")
         exit(1)
 
 
 #Login to router
 child.sendline(router_pass)
 child.expect('>')
-child.sendline('en')
-child.expect('Password:')
-child.sendline(router_pass)
-child.expect('#')
-#child.sendline('conf t')
-#child.expect('#')
+syslog.syslog("Router login successful")
 
 #Run backup
+syslog.syslog("Starting FTP backup")
 child.sendline('copy run ftp')
 child.expect("\?")
 child.sendline('hcusyslog')
 child.expect('\?')
-child.sendline(router+"-"+time.strftime("%d.%m.%Y")+".cfg")
-
-
-
+child.sendline(backup_full_path)
 
 #Log out of router
+syslog.syslog("Logging out of the router")
 child.sendline('exit')
-child.expect('#')
+child.expect('>')
 child.sendline('exit')
 
+#Verify that the created backup file now exists
+if os.stat(ftp_backup_location+router+"/"+backup_file_name) > 0:
+        print("Backup was successful")
+        syslog.syslog("Backup was successful. Located at " + ftp_backup_location+router+"/"+backup_file_name)
+
+
+else:
+        print("Backup file was less that 0 byes. The backup probably failed")
+        syslog.syslog("Backup file was less that 0 byes. The backup probably failed")
 
 #Exit
-print ("Program exited successfully, this did not mean the backup was successful")
 exit (0)
